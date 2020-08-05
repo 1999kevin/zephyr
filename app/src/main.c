fbfd0592875744a82e584dfc312dc9b5a3c24ff9
keliu <3160104016@zju.edu.cn>
2020-08-05 18:50:54 +0800

    bluetooth read function achieve

--- app/src/main.c
+++ app/src/main.c
@@ -13,17 +13,10 @@
 #include <sys/printk.h>
 #include <device.h>
 #include <drivers/uart.h>
+#include "BT.h"
 
 
 // uint8_t* buf = "deliver message\n";
-
-const struct uart_config cfg = {
-	.baudrate = 115200,
-	.parity = UART_CFG_PARITY_NONE,
-	.stop_bits = UART_CFG_STOP_BITS_1,
-	.data_bits = UART_CFG_DATA_BITS_8,
-	.flow_ctrl = UART_CFG_FLOW_CTRL_NONE
-};
 
 struct voltage_message {
 	/* data */
@@ -35,76 +28,96 @@ struct voltage_message {
 	uint16_t EOF;
 };
 
+struct read_in_buffer {
+	uint8_t *buf;
+	int size;
+};
 
-void uart_poll_out_multi(struct device *dev, unsigned char* str, int len);
-int compose_message(struct voltage_message *msg, uint16_t voltage);
-void voltage_message_format_convert(struct voltage_message *msg, uint8_t *buf);
 
+void uart_fifo_callback(struct device *dev);
+int uart_fifo_init(void);
+
+
+const struct uart_config cfg = {
+	.baudrate = 115200,
+	.parity = UART_CFG_PARITY_NONE,
+	.stop_bits = UART_CFG_STOP_BITS_1,
+	.data_bits = UART_CFG_DATA_BITS_8,
+	.flow_ctrl = UART_CFG_FLOW_CTRL_NONE
+};
+
+struct device *dev;
+
+uint8_t read_in_buf[20]={0};
+
+struct read_in_buffer buf = {
+	.buf = read_in_buf,
+	.size = 0
+};
 
 void main(void)
 {
-	struct device *dev;
-	// Usart2_SendStr("deliver message\n");
+	uart_fifo_init();
+	printk("here\n");
+
+}
+
+int uart_fifo_init(void){
+	uint8_t c;
 	dev = device_get_binding("UART_2");
 	if (dev == NULL) {
 		printk("cannot find device\n");
-		return;
+		return -1;
 	}
-
-	struct voltage_message msg;
-
-	compose_message(&msg,233);
-	
 	if(uart_configure(dev, &cfg) != 0){
 		printk("configure error\n");
 	}
 	printk("configure succuessfully\n");
-	// int ret = uart_tx(dev, buf, 10, SYS_FOREVER_MS);
-	// if (ret!=0){
-	// 	printk("error code: %d\n",ret);
-	// }else{
-	// 	printk("write data successfully\n");
-	// }
-	unsigned char buf[15]={0};
-	voltage_message_format_convert(&msg,buf);
-	// uart_poll_out(dev, 'a');
-	printk("char 0: %d\n",*buf);
-	uart_poll_out_multi(dev,buf, msg.len);
-	printk("send out telegraph\n");
+		
+	uart_irq_callback_set(dev, uart_fifo_callback);
 
-}
-
-int compose_message(struct voltage_message *msg, uint16_t voltage){
-	msg->SOF = 0xb1b1;
-	msg->len = 11;
-	msg->cmd = 0;
-	msg->data = voltage;
-	msg->crc = msg->cmd+msg->data+msg->len;
-	msg->EOF = 0x1b1b;
-	printk("msg->SOF:%d\n",msg->SOF);
-	return 0;
-}
-
-
-void uart_poll_out_multi(struct device *dev, unsigned char* str, int len){
-	for (int i=0; i<len;i++){
-		uart_poll_out(dev, *str++);
+	/* Drain the fifo */
+	if (uart_irq_rx_ready(dev)) {
+		while (uart_fifo_read(dev, &c, 1)) {
+			;
+		}
 	}
+	uart_irq_rx_enable(dev);
+
+	/* Enable all interrupts unconditionally. Note that this is due
+	 * to Zephyr issue #8393. This should be removed once the
+	 * issue is fixed in upstream Zephyr.
+	 */
+	irq_unlock(0);
+
+	return 0;
+
 }
 
-void voltage_message_format_convert(struct voltage_message *msg, unsigned char* buf){
-	*buf = (unsigned char)((msg->SOF >> 8) & 0xFF);
-	*(buf+1) = (unsigned char)((msg->SOF) & 0xFF);
-	*(buf+2) = (unsigned char)((msg->len >> 8) & 0xFF);
-	*(buf+3) = (unsigned char)((msg->len) & 0xFF);
-	*(buf+4) = (unsigned char)((msg->cmd >> 8) & 0xFF);
-	*(buf+5) = (unsigned char)((msg->cmd) & 0xFF);
-	*(buf+6) = (unsigned char)((msg->data >> 8) & 0xFF);
-	*(buf+7) = (unsigned char)((msg->data) & 0xFF);
-	*(buf+8) = (unsigned char)(msg->crc);
-	*(buf+9) = (unsigned char)((msg->EOF >> 8) & 0xFF);
-	*(buf+10) = (unsigned char)((msg->EOF) & 0xFF);
+void uart_fifo_callback(struct device *dev){
+	printk("come to callback\n");
+	uint8_t byte[4];
+	int rx;
 
-	printk("buf[0]:0x%02x \n",buf[0]);
+	uart_irq_update(dev);
+
+	if (!uart_irq_rx_ready(dev)) {
+		return;
+	}
+	// int i=0;
+	printk("ready to read\n");
+	while (true) {
+		rx = uart_fifo_read(dev, &buf.buf[buf.size], 1);
+		printk("rx:%d\n",rx);
+		if (rx != 1) {
+			break;
+		}
+	}
+	buf.size++;
+	printk("callback end\n");
+	printk("byte: 0x%02x 0x%02x 0x%02x\n",buf.buf[0], buf.buf[1],buf.buf[2]);
+	// printk("byte :0x%02x, 0x%02x,0x%02x,0x%02x\n",read_in_buf[0],read_in_buf[1],read_in_buf[2],read_in_buf[3]);
+
 }
+
 
