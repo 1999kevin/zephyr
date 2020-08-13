95f45591680154291a749cd2e83a14e3ffab6cda
keliu <3160104016@zju.edu.cn>
2020-08-13 15:43:42 +0800

    combine voltage, pwm and bluetooth

--- app/src/main.c
+++ app/src/main.c
@@ -10,9 +10,11 @@
  */
 
 #include <zephyr.h>
-#include "BT.h"
 #include <drivers/pwm.h>
 #include <drivers/gpio.h>
+#include "BT.h"
+#include "main.h"
+
 
 
 // uint8_t* buf = "deliver message\n";
@@ -34,11 +36,12 @@ void main(void)
 	struct device *dev_GPIOB = device_get_binding("GPIOB");
 	struct device *dev_GPIOA = device_get_binding("GPIOA");
 	struct device *dev_UART2 = device_get_binding("UART_2");
+	struct device *dev_ADC1 = device_get_binding("ADC_1");
 	bool led_is_on = true;
 	int ret;
 
-	if (dev_GPIOB == NULL || dev_GPIOA == NULL) {
-		printk("cannnot find GPIOA or GPIOB\n");
+	if (dev_GPIOB == NULL || dev_GPIOA == NULL || dev_UART2 == NULL || dev_ADC1 == NULL) {
+		printk("cannnot find GPIOA or GPIOB or UART2 or ADC1\n");
 		return;
 	}
 
@@ -74,62 +77,179 @@ void main(void)
 	ret = gpio_pin_set(dev_GPIOA, 5, 1);
 	printk("bluetooth mode:%d\n",ret);
 
+	/* set BAT_EN on */
+	ret = gpio_pin_configure(dev_GPIOA, 1, GPIO_OUTPUT_ACTIVE | FLAGS);
+	if (ret < 0) {
+		printk("get out from here\n");
+		return;
+	}
+	ret = gpio_pin_set(dev_GPIOA, 1, 1);
+	printk("BAT_EN:%d\n",ret);
 
+
+
+
+	/* initiate ring buf */
     ring_buf_init(&telegram_queue.rb, MY_RING_BUF_SIZE , telegram_queue.buffer);
 	uart_fifo_init();
 
-	uint8_t data[20];
-	// uint8_t *print_data = data;
-	int size;
+
+	/* wait for bluetooth to connect */
+	// int size;
+	uint8_t* ask_msg = "AT:RSSI?\r\n";
+
 	while(1){
-		printk_buf_str(buf);
-		printk("buf.size:%d\n",buf.size);  
-		size = pull_one_message(data);
-		// printk("ret:%d\n",size);
-		if(size>0){
-			printk("get data: %d\n",data[0]);
-			printk("size: %d\n",size);
+		ret = gpio_pin_get(dev_GPIOA, 6);
+		if(ret == 0){
 			break;
-		}else{
-			// printk("waiting for data, free space:%d\n",ring_buf_space_get(&telegram_queue.rb));
 		}
-		k_msleep(5000);
+		else{
+			printk("bluetooth not connected\n");
+		}
+		// uart_poll_out_multi(dev_UART2, ask_msg, 10);
+		k_msleep(1000);
+		// printk_buf_str(buf);
+		// k_msleep(2000);
+		// printk_buf_str(buf);
+
 	}
 
 
-	if(size == 11){
-		printk("get a voltage message\n");
-		uint16_t voltage = data[6]*16*16+data[7];
-		printk("voltage:%d\n", voltage);
-	}else if(size == 15){
-		printk("get a pwm message\n");
-		uint16_t period = data[6]*16*16+data[7];
-		uint16_t pulse = data[8]*16*16+data[9];
-		uint16_t duration = data[10]*16*16+data[11];
-
-		printk("period: %d, pulse: %d, duration: %d\n", period, pulse, duration);
-
-
-		// uint32_t period;
-		int ret;
-		// printk("PWM-based blinky\n");
-		const char* label4 = "PWM_4";
-		struct device *dev_pwm4 = device_get_binding(label4);
-		if (!dev_pwm4) {
-			printk("Error: didn't find %s device\n", label4);
-			return;
-		}
-		printk("%s correct\n", label4);
-
-		uint64_t cycles;
-		ret = pwm_get_cycles_per_sec(dev_pwm4,1, &cycles);
-
-		printk("clock rate: %lld\n",cycles);
-		ret = pwm_pin_set_usec(dev_pwm4,1, period, pulse, PWM_FLAGS);
-		if(ret < 0){
-			printk("error %d\n",ret);
-		}
-		printk("set %s,channel 1, successfully\n",label4);
+	/* obtain voltage meeage */
+	int voltage_result = 0;
+	ret = adc_channel_setup(dev_ADC1, &adc_ch_cfg);
+	// printk("here2\n");
+	if (ret != 0) {
+		printk("Setting up of adc channel failed with code %d\n", ret);
+		return;
 	}
+	// printk("here2\n");
+	if(adc_read(dev_ADC1,&sequence) < 0){
+		printk("cannot read value\n");
+	}else{
+		float result = buffer[0];
+		// printk("voltage:");
+		voltage_result = (int)(result*3300/4096);
+		// printk("mV\n");
+	}
+
+	/* transmit voltage message */
+	struct voltage_message msg;
+
+	compose_message(&msg,voltage_result);
+
+	unsigned char voltage_buf[15]={0};
+	voltage_message_format_convert(&msg,voltage_buf);
+	// uart_poll_out(dev, 'a');
+	printk("char 0: %d\n",*voltage_buf);
+	uart_poll_out_multi(dev_UART2,voltage_buf, msg.len);
+	printk("send out telegraph\n");
+
+
+	// uint8_t data[20];
+	// uint8_t* msg = "AT:MAC?\r\n";
+
+	// int size;
+	// while(1){
+
+	// 	// uart_poll_out_multi(dev_UART2, msg, 9);
+	// 	// k_msleep(100);
+	// 	// printk_buf_str(buf);
+	// // 	// k_msleep(2000);
+	// // 	// printk_buf_str(buf);
+	// // 	// printk("buf.size:%d\n",buf.size);  
+	// 	size = pull_one_message(data);
+	// 	// printk("ret:%d\n",size);
+	// 	if(size>0){
+	// 		printk("get data: %d\n",data[0]);
+	// 		printk("size: %d\n",size);
+	// 		break;
+	// 	}else{
+	// 		// printk("waiting for data, free space:%d\n",ring_buf_space_get(&telegram_queue.rb));
+	// 	}
+	// 	printk_buf_str(buf);
+	// 	k_msleep(5000);
+
+	// }
+
+
+	// if(size == 11){
+	// 	printk("get a voltage message\n");
+	// 	uint16_t voltage = data[6]*16*16+data[7];
+	// 	printk("voltage:%d\n", voltage);
+	// }else if(size == 15){
+	// 	printk("get a pwm message\n");
+	// 	uint16_t period = data[6]*16*16+data[7];
+	// 	uint16_t pulse = data[8]*16*16+data[9];
+	// 	uint16_t duration = data[10]*16*16+data[11];
+
+	// 	printk("period: %d, pulse: %d, duration: %d\n", period, pulse, duration);
+
+
+	// 	// uint32_t period;
+	// 	int ret;
+	// 	// printk("PWM-based blinky\n");
+	// 	const char* label4 = "PWM_4";
+	// 	struct device *dev_pwm4 = device_get_binding(label4);
+	// 	if (!dev_pwm4) {
+	// 		printk("Error: didn't find %s device\n", label4);
+	// 		return;
+	// 	}
+	// 	printk("%s correct\n", label4);
+
+	// 	uint64_t cycles;
+	// 	ret = pwm_get_cycles_per_sec(dev_pwm4,1, &cycles);
+
+	// 	printk("clock rate: %lld\n",cycles);
+	// 	ret = pwm_pin_set_usec(dev_pwm4,1, period, pulse, PWM_FLAGS);
+	// 	if(ret < 0){
+	// 		printk("error %d\n",ret);
+	// 	}
+	// 	printk("set %s,channel 1, successfully\n",label4);
+	// }
 	
 }
+
+
+
+void PrintFloat(float value){
+	int tmp,tmp1,tmp2,tmp3,tmp4,tmp5,tmp6;
+	tmp = (int)value;
+	tmp1=(int)((value-tmp)*10)%10;
+	tmp2=(int)((value-tmp)*100)%10;
+	tmp3=(int)((value-tmp)*1000)%10;
+	tmp4=(int)((value-tmp)*10000)%10;
+	tmp5=(int)((value-tmp)*100000)%10;
+	tmp6=(int)((value-tmp)*1000000)%10;
+	printk("%d.%d%d%d%d%d%d ",tmp,tmp1,tmp2,tmp3,tmp4,tmp5,tmp6);
+
+}
+
+
+void voltage_message_format_convert(struct voltage_message *msg, unsigned char* buf){
+	*buf = (unsigned char)((msg->SOF >> 8) & 0xFF);
+	*(buf+1) = (unsigned char)((msg->SOF) & 0xFF);
+	*(buf+2) = (unsigned char)((msg->len >> 8) & 0xFF);
+	*(buf+3) = (unsigned char)((msg->len) & 0xFF);
+	*(buf+4) = (unsigned char)((msg->cmd >> 8) & 0xFF);
+	*(buf+5) = (unsigned char)((msg->cmd) & 0xFF);
+	*(buf+6) = (unsigned char)((msg->data >> 8) & 0xFF);
+	*(buf+7) = (unsigned char)((msg->data) & 0xFF);
+	*(buf+8) = (unsigned char)(msg->crc);
+	*(buf+9) = (unsigned char)((msg->EOF >> 8) & 0xFF);
+	*(buf+10) = (unsigned char)((msg->EOF) & 0xFF);
+
+	printk("buf[0]:0x%02x \n",buf[0]);
+}
+
+int compose_message(struct voltage_message *msg, uint16_t voltage){
+	msg->SOF = 0xb1b1;
+	msg->len = 11;
+	msg->cmd = 0;
+	msg->data = voltage;
+	msg->crc = msg->cmd+msg->data+msg->len;
+	msg->EOF = 0x1b1b;
+	printk("msg->SOF:%d\n",msg->SOF);
+	return 0;
+}
+
---
+++ app/src/main.h
@@ -0,0 +1,50 @@
+#ifndef __MAIN_H_
+#define __MAIN_H_
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#include <drivers/adc.h>
+#include <sys/printk.h>
+#include <device.h>
+
+void PrintFloat(float value);
+int compose_message(struct voltage_message *msg, uint16_t voltage);
+void voltage_message_format_convert(struct voltage_message *msg, uint8_t *buf);
+
+static const struct adc_channel_cfg adc_ch_cfg = {
+	.gain  = ADC_GAIN_1,
+	.reference = ADC_REF_INTERNAL,
+	.acquisition_time = ADC_ACQ_TIME_DEFAULT,
+	.channel_id = 1,
+	.differential  = 0
+};
+
+uint16_t buffer[100];
+
+const struct adc_sequence sequence = {
+	.options = NULL,
+	.channels = BIT(1),
+	.buffer = buffer,
+	.buffer_size = 100,
+	.resolution = 12
+};
+
+// struct voltage_message {
+// 	/* data */
+// 	uint16_t SOF;
+// 	uint16_t len;
+// 	uint16_t cmd;
+// 	uint16_t data;
+// 	uint8_t crc;
+// 	uint16_t EOF;
+// };
+
+
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* __MAIN_H_ */
