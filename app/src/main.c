e9b92d422b3e1da477e0b3702d2d2d5c621303f3
keliu <3160104016@zju.edu.cn>
2020-08-18 14:31:07 +0800

    open all function

--- app/src/main.c
+++ app/src/main.c
@@ -21,61 +21,61 @@ void main(void)
 		printk_buf_str(buf);
 
 	}
-	// /* wait for bluetooth to connect */
-	// while(!bluetooth_is_connected()){
-	// 	printk("waiting for bluetooth to connect\n");
-	// 	k_msleep(1000);
-	// }
-	// printk("bluetooth connected\n");
-	// k_msleep(5000);
+	/* wait for bluetooth to connect */
+	while(!bluetooth_is_connected()){
+		printk("waiting for bluetooth to connect\n");
+		k_msleep(1000);
+	}
+	printk("bluetooth connected\n");
+	k_msleep(5000);
 
 
-	// /* obtain voltage message */
-	// float voltage_result = get_voltage();
-	// if(voltage_result<0){
-	// 	return;
-	// }
+	/* obtain voltage message */
+	float voltage_result = get_voltage();
+	if(voltage_result<0){
+		return;
+	}
 
-	// // /* transmit voltage message */
-	// transmit_voltage_message((int)voltage_result);
+	// /* transmit voltage message */
+	transmit_voltage_message((int)voltage_result);
 
 
-	// /* wait for message */
-	// uint8_t data[20];
-	// // uint8_t* msg = "AT:MAC?\r\n";
+	/* wait for message */
+	uint8_t data[20];
+	// uint8_t* msg = "AT:MAC?\r\n";
 
-	// int size;
-	// while(1){
-	// 	size = pull_one_message(data);
-	// 	if(size>0){
-	// 		break;
-	// 	}else{
-	// 		printk("waiting for data\n");
-	// 	}
-	// 	printk_buf_str(buf);
-	// 	k_msleep(5000);
+	int size;
+	while(1){
+		size = pull_one_message(data);
+		if(size>0){
+			break;
+		}else{
+			printk("waiting for data\n");
+		}
+		printk_buf_str(buf);
+		k_msleep(5000);
 
-	// }
+	}
 
-	// /* set led power on */
-	// set_led_power_on();
+	/* set led power on */
+	set_led_power_on();
 
-	// /* decode message */
-	// uint16_t period = 0;
-	// uint16_t pulse = 0;
-	// uint16_t duration = 0;
-	// uint16_t voltage = 0;
+	/* decode message */
+	uint16_t period = 0;
+	uint16_t pulse = 0;
+	uint16_t duration = 0;
+	uint16_t voltage = 0;
 
-	// if(size == 11){
-	// 	decode_voltage_message(data, &voltage);
-	// }else if(size == 15){
-	// 	decode_pwm_message(data, &period, &pulse, &duration);
-	// }
+	if(size == VOLTAGE_MESSAGE_LENGTH){
+		decode_voltage_message(data, &voltage);
+	}else if(size == PWM_MESSAGE_LENGTH){
+		decode_pwm_message(data, &period, &pulse, &duration);
+	}
 
-	// /* set pwm to led */
-	// ret = set_pwm_to_led(period, pulse, duration);
-	// if(ret<0){
-	// 	printk("do not set pwm\n");
-	// 	return;
-	// }
+	/* set pwm to led */
+	ret = set_pwm_to_led(period, pulse, duration);
+	if(ret<0){
+		printk("do not set pwm\n");
+		return;
+	}
 }
